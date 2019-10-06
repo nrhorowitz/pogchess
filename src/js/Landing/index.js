9c2df182f884231453187864a833271836022193
Nathan Horowitz <nhorowitz@berkeley.edu>
2019-10-05 17:33:24 -0700

    RESPONSIVE UI for signup page

--- src/js/Landing/index.js
+++ src/js/Landing/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Leaderboard from './Leaderboard.js';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
 
 
@@ -18,6 +19,8 @@ class Landing extends React.Component {
             if (true) { //TODO: check token
                 this.setState({redirect: '/login'});
             }
+        } else if (type === "SignUp") {
+            
         }
     }
 
@@ -31,6 +34,9 @@ class Landing extends React.Component {
                 <div>
                     <Typography variant="h1">LANDING</Typography>
                     <Button variant="contained" color="secondary" onClick={()=>this.resolveClick("Login")}>LOGIN</Button>
+                    <Button variant="contained" color="secondary" onClick={()=>this.resolveClick("SignUp")}>SIGNUP</Button>
+                    <Button variant="contained" color="secondary" onClick={()=>this.resolveClick("PlayAsGuest")}>PLAY AS GUEST</Button>
+                    <Leaderboard></Leaderboard>
                 </div>
             )
         }
--- src/js/Login/index.js
+++ src/js/Login/index.js
@@ -11,21 +11,78 @@ class Login extends React.Component {
         super(props);
         this.state = {
             redirect: '',
+            inputEmail: '',
+            inputPassword: '',
+            inputEmailError: false,
+            inputPasswordError: false,
+            errorMessage: '',
         };
         this.renderMessage = this.renderMessage.bind(this);
         this.resolveClick = this.resolveClick.bind(this);
     }
 
     renderMessage() {
-        return (
-            <div className='incorrect-field'>&#160;</div>
-        )
+        if (this.state.errorMessage === "") {
+            return (
+                <Typography variant="h3">&#160;</Typography>
+            )
+        } else {
+            return (
+                <Typography variant="h6">{this.state.errorMessage}</Typography>
+            )
+        }
+    }
+
+    validInput(type) {
+        if (type === "email") {
+            //TODO: create actual email checker 
+            if (this.state.inputEmail.length < 8) {
+                this.setState({inputEmailError: true});
+                this.setState({errorMessage: "Email must be longer than 7 characters"});
+                return false;
+            } else {
+                this.setState({inputEmailError: false});
+                return true;
+            }
+        } else if (type === "password") {
+            if (this.state.inputPassword.length < 6) {
+                this.setState({inputPasswordError: true});
+                this.setState({errorMessage: "Password must be longer than 5 characters"});
+                return false;
+            } else {
+                this.setState({inputPasswordError: false});
+                return true;
+            }
+        }
     }
 
     resolveClick(type) {
-        if (type === "SignUp") {//TODO: check token
-            this.setState({redirect: '/signup'});
+        if (type === "Login") {
+            if (this.validInput("email") && this.validInput("password")) {
+                console.log("OK");
+            }
         }
+        if (type === "SignUp") {//TODO: check token
+            //this.setState({redirect: '/signup'});
+            if (this.validInput("email") && this.validInput("password")) {
+                this.signUpUser();
+            }
+        }
+    }
+
+    signUpUser() {
+        this.props.firebase.auth().createUserWithEmailAndPassword(this.state.inputEmail, this.state.inputPassword).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            console.log("ERRORCODE: ", errorCode);
+            console.log("ERRORMESSAGE: ", errorMessage);
+            if (errorCode === "auth/invalid-email") {
+                this.setState({inputEmailError: true});
+                this.setState({errorMessage: "Email must be a valid email"});
+            }
+        });
     }
     
     render() {
@@ -38,24 +95,33 @@ class Login extends React.Component {
                 <div className='log-page'>
                     <div className='log-content'>
                         {/*<div className='log-logo'><img src={logo}/></div>*/}
-                        <Typography variant='h1'>POGCHAT</Typography>
-                        <div className='log-header'>Please log in to continue</div>
+                        <Typography variant='h1'>WELCOME UWU</Typography>
                         <div className='log-field-container'>
                             <div className='log-input-container'>
-                                <TextField label='USERNAME/EMAIL'></TextField>
+                                <TextField 
+                                    label='USERNAME/EMAIL' 
+                                    variant="outlined" 
+                                    fullWidth 
+                                    error={this.state.inputEmailError} 
+                                    value={this.state.inputEmail}
+                                    onChange={(e)=>this.setState({inputEmail: e.target.value})}
+                                ></TextField>
                             </div>
                             <div className='log-input-container'>
-                                <TextField label='PASSWORD'></TextField>
+                                <TextField 
+                                    label='PASSWORD' 
+                                    variant="outlined" 
+                                    fullWidth 
+                                    error={this.state.inputPasswordError}
+                                    value={this.state.inputPassword}
+                                    onChange={(e)=>this.setState({inputPassword: e.target.value})}
+                                ></TextField>
                             </div>
                         </div>
                         {/*<div className='log-forgot-password'>Forgot password?</div>*/}
+                        <Button variant='contained' color="primary" onClick={()=>this.resolveClick("Login")}>LOGIN</Button>
+                        <Button variant='contained' color="primary" onClick={()=>this.resolveClick("SignUp")}>CREATE ACCOUNT</Button>
                         {this.renderMessage()}
-                        <Button variant='contained'>LOGIN</Button>
-                        <Button variant='contained' onClick={()=>this.resolveClick("SignUp")}>SIGNUP</Button>
-                        <div className='log-footer-area'>
-                            <div className='log-footer'>Don't have an account? <Link to="/signup?id=1"><span className='link'>Join us</span></Link></div>
-                            <div className='log-footer'>By continuing, you agree to the <br/><Link to="/TOS"><span className='link'>Terms of Service</span></Link> and <Link to="/privacy"><span className='link'>Privacy Policy</span></Link></div>
-                        </div>
                     </div>
                 </div>
             )
